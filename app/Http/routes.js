8f7c1ccde2f46ffd1b465d6ea1f8d11538ed5f1f
t0msky <syahrunsulaiman@gmail.com>
2017-05-16 20:46:50 +0800

    This is where it all begins...

---
+++ app/Http/routes.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Router
+|--------------------------------------------------------------------------
+|
+| AdonisJs Router helps you in defining urls and their actions. It supports
+| all major HTTP conventions to keep your routes file descriptive and
+| clean.
+|
+| @example
+| Route.get('/user', 'UserController.index')
+| Route.post('/user', 'UserController.store')
+| Route.resource('user', 'UserController')
+*/
+
+const Route = use('Route')
+
+// Main Page
+Route.on('/').render('welcome')
+
+// Login
+Route.get('/login', 'AuthController.index')
+Route.post('/login', 'AuthController.login')
+
+// Register
+Route.get('/register', 'RegisterController.index')
+Route.post('register', 'RegisterController.doRegister')
