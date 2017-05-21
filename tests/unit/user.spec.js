13d280ffa88a26c3371e51c2a3df514d8e30f1dc
Da Few <1ofdafew@gmail.com>
2017-05-21 16:29:44 +0800

    Fixed reporter

---
+++ tests/unit/user.spec.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const chai = use('chai')
+const assert = chai.assert
+const User = use('App/Model/User')
+require('co-mocha')
+
+describe('User', function() {
+  
+  it('should be able to register user', function() {
+    const user = new User()
+    user.email = 'foo@bar.com'
+    user.password = 'secret'
+
+    const res = user.save()
+    assert.instanceOf(res, User)
+    // assert.equal(res.status, 'pending-verification')
+    // assert.match(res.verification_code, /[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12}/)    
+  })
+          
+})
