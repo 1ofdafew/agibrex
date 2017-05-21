2ba3b378b4661bba31d0bdfcaf8b4d0104c91782
Da Few <1ofdafew@gmail.com>
2017-05-22 05:43:47 +0800

    Done for auth API tests

--- tests/functional/api-test.spec.js
+++ tests/functional/api-test.spec.js
@@ -23,7 +23,7 @@ describe('API Test', function() {
 
   it('shall allow us to register user', function() {
     const data = {
-      username: 'hisham',
+      username: 'mhishami',
       email: 'mhishami@gmail.com',
       password: 'secret'
     }
@@ -45,7 +45,47 @@ describe('API Test', function() {
       })
   })
 
-  it('shall give us the user details') 
-  it('shall give us the authentication token') 
+  it('shall give us the user details', function() {
+    chai.request(URL)
+      .get('/auth/mhishami')
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.a('object')
+        res.body.should.have.property('ok')
+        res.body.ok.should.have.property('uuid')
+        res.body.ok.should.have.property('username')
+        res.body.ok.should.have.property('email')
+        res.body.ok.should.have.property('mobile_no')
+        res.body.ok.should.have.property('password')
+        res.body.ok.should.have.property('created_at')
+        res.body.ok.should.have.property('updated_at')
+        done()
+      })    
+  })
+
+  it('shall give us the authentication token', function() {
+    chai.request(URL)
+      .post('/auth')
+      .send({ passwd: 'secret' })
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.a('object')
+        res.body.should.have.property('status').eql('ok')
+        res.body.should.have.property('data')
+        res.body.data.should.have.property('token')
+      })    
+  }) 
+
+  it('shall give us the authentication error', function() {
+    chai.request(URL)
+      .post('/auth')
+      .send({ passwd: 'badpass' })
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.a('object')
+        res.body.should.have.property('status').eql('error')
+        res.body.should.have.property('message').eql('Invalid username, or password')
+      })    
+  }) 
 
 })
