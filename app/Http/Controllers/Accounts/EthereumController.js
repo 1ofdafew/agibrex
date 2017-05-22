c3e8330502f0cc6c77dc71d14b8c5734af6d2a3f
Da Few <1ofdafew@gmail.com>
2017-05-23 03:16:28 +0800

    Removed that pesky invalid login

--- app/Http/Controllers/Accounts/EthereumController.js
+++ app/Http/Controllers/Accounts/EthereumController.js
@@ -1,25 +1,19 @@
 'use strict'
 const Database = use('Database')
 const OrderBook = use('App/Model/OrderBook')
-const EtherCoin = use('App/Services/Coins/Ethereum')
 
 class EthereumController {
 
-  * account (request, response) {
-
-
-      const etherAcc = new EtherCoin()
-      yield etherAcc.checkBalance('0xd5956c61571bb5d34e6db25290200d4572444581')
-      response.ok(etherAcc)
-      yield response.sendView('accounts/ethereum', { type: 'ethereum'})
+  * account(request, response) {
+    yield response.sendView('accounts/ethereum', { type: 'ethereum' })
   }
 
-  * deposit (request, response) {
-      yield response.sendView('accounts/deposit/ethereum', { type: 'ethereum'})
+  * deposit(request, response) {
+    yield response.sendView('accounts/deposit/ethereum', { type: 'ethereum' })
   }
 
-  * withdraw (request, response) {
-      yield response.sendView('accounts/withdraw/ethereum', { type: 'ethereum'})
+  * withdraw(request, response) {
+    yield response.sendView('accounts/withdraw/ethereum', { type: 'ethereum' })
   }
 
 }
--- tests/unit/coins-unit-test.spec.js
+++ tests/unit/coins-unit-test.spec.js
@@ -46,6 +46,22 @@ describe('Coins Unit Test', function() {
     assert.notEqual(balance, null)
   })
 
+  it('ethereum: should be able to create account, and get balance', function * () {
+    this.timeout(10000)
+
+    const factory = new CoinFactory('ethereum')
+    const resp = yield factory.createWallet('foo', '1234')
+    
+    assert.equal(resp.data.wallet.type, 'ETHEREUM')
+
+    const addr = resp.data.wallet.address
+    const balance = yield factory.getBalance(addr)
+
+    console.log('Balance:', balance)
+    assert.notEqual(addr, null)
+    assert.notEqual(balance, null)
+  })
+
   it('should delete the user admin', function * () {
     this.timeout(10000)    
     const user = yield APIAuthService.delete(ADMIN)
