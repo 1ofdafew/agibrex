35177434c258bf3a444e1f261c148601087f2044
t0msky <syahrunsulaiman@gmail.com>
2017-05-22 18:38:00 +0800

    Update account

--- app/Http/Controllers/Accounts/TractoController.js
+++ app/Http/Controllers/Accounts/TractoController.js
@@ -1,8 +1,15 @@
 'use strict'
 
+const TractoCoin = use('App/Services/Coins/TractoCoin')
+
 class TractoController {
 
   * account (request, response) {
+
+    const tractoAcc = new TractoCoin()
+    yield tractoAcc.checkBalance('TvxTwZhrxaHV5GnfZgSfiLX5XmfVR4fB5dBAzm91AW7cDfShT19AFno7gMhZp1ejdJdxBKkRTnQ8RNDxB2fjDqg32gQR7zsDp')
+    response.send(tractoAcc)
+
     yield response.sendView('accounts/tracto', { type: 'tracto'})
   }
 
--- app/Services/Coins/TractoCoin.js
+++ app/Services/Coins/TractoCoin.js
@@ -2,9 +2,10 @@
 
 const got = require('got');
 const Coin = use('App/Services/Coins/Coin')
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.IntcInVzZXJuYW1lXCI6XCJnaWJyZXhcIixcImVtYWlsXCI6XCJhZG1pbkBnaWJyZXguY29tXCJ9Ig.0GkMJHKkOoJB5J_tDrykQysOiTDFXFiscZ6bmVgLSM4';
 
 class TractoCoin extends Coin {
-  
+
   constructor() {
     super()
   }
@@ -24,6 +25,19 @@ class TractoCoin extends Coin {
       // console.log(res.body)
       return JSON.stringify(res.body)
     })
+  }
+
+  * checkBalance(wallet_address) {
+      console.log(`Checking balance for ${wallet_address}...`)
+      return got.get(`http://158.69.170.180:8080/api/v1/tracto/${wallet_address}`, {
+          headers: {
+              'content-type': 'application/json',
+              'x-auth': token
+          },
+          json: true
+      }).then(res => {
+        return JSON.stringify(res.body)
+      })
   }
 
 }
