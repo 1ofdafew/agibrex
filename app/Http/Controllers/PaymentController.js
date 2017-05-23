ddcb3f2abe771973765c4dd0b88087b265b706a9
ifa <iefaez22102011@gmail.com>
2017-05-23 11:23:55 +0800

    dw

--- app/Http/Controllers/PaymentController.js
+++ app/Http/Controllers/PaymentController.js
@@ -6,28 +6,20 @@ const Database = use('Database');
 class PaymentController {
 
 * index (request, response) {
-      // yield response.sendView('profile')
-      // response.ok(profile)
-      //const profile = yield Profile.all()
-       const payment = yield Database.table('payment')
+
+      const payment = yield Database.table('payments')
       response.json(payment)
     }
   
 
     * store(request, response) {
-      const data = request.only(['', '', '', ''])
-      console.log('ProfileController data....')
+      const data = request.only(['trans_id', 'amount', 'type'])
+      console.log('PaymentController data....')
       console.log(data)
 
           const payment = new Payment(data)
           yield payment.save()
           response.ok(payment)
-
-          // var profileMessage = {
-          //     success: 'already save'
-          // }
-
-          // yield response.sendView('profile', { profileMessage : profileMessage })
       }
 
       * show(request, response) {
@@ -36,19 +28,10 @@ class PaymentController {
 
         
         const data = yield Database
-        .table('payment')
-        .select('', '', '', '')
+        .table('payments')
+        .select('trans_id', 'amount', 'type')
         .where({id:id})
-        // .where({id:id});
-        // const profiles = data[0]
 
-        // var request = require('request');
-        // request(article.href, function (error, response, body) {
-        //   if(!error){
-        //     console.log(body)
-        //   }
-        // })
-        // return profiles
         response.json(data)
 
         }
