555bfca44db76aabfa7ba258b79d762aa1265225
Da Few <1ofdafew@gmail.com>
2017-05-25 02:10:45 +0800

    Fixed invalid verification code

--- app/Http/Controllers/Auth/AccountVerifyController.js
+++ app/Http/Controllers/Auth/AccountVerifyController.js
@@ -28,19 +28,25 @@ class AccountVerifyController {
   }
 
   * verify (request, response) {
-    const token = request.input('token')
-    console.log('Auth Token:', token)
-    const user = yield UserService.findByOrFail('verification_code', token)
 
-    user.status = 'active'
-    const updateUser = yield user.save()
+    try {
+      const token = request.input('token')
+      console.log('Auth Token:', token)
+      const user = yield UserService.findByOrFail('verification_code', token)
 
-    if (!updateUser) {
+      user.status = 'active'
+      const updateUser = yield user.save()
+
+      if (!updateUser) {
+        yield request.with({error: 'Unable to activate your account. Invalid Verification Code'}).flash()
+        response.redirect('verify')
+      } else {
+        yield request.with({info: 'Account activated. Please login'}).flash()
+        response.redirect('login')
+      }
+    } catch (e) {
       yield request.with({error: 'Unable to activate your account. Invalid Verification Code'}).flash()
       response.redirect('verify')
-    } else {
-      yield request.with({info: 'Account activated. Please login'}).flash()
-      response.redirect('login')
     }
   }
 
