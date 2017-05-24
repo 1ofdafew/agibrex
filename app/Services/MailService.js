3ca668d68c9c31a347b0edc4416b69f0a9de8499
Da Few <1ofdafew@gmail.com>
2017-05-25 01:43:29 +0800

    Fixed the email sending, and account verification

--- app/Services/MailService.js
+++ app/Services/MailService.js
@@ -19,7 +19,7 @@ MailService.sendVerificationEmail = function * (user) {
   if (!user || typeof (user.toJSON) !== 'function') {
     throw new Error('Mailer expects a valid instance of User Model.')
   }
-  return yield Mail.send('emails.user-verification', user.toJSON(), (message) => {
+  return yield Mail.send('emails.userVerification', user.toJSON(), (message) => {
     message.to(user.email, user.name)
     message.from(Env.get('MAIL_FROM_EMAIL'), Env.get('MAIL_FROM_NAME'))
     message.subject('Verify Gibrex Account Email Address')
