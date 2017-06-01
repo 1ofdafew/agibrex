0a441c28db2469aff91149c870af5441a6e5cc1c
Da Few <1ofdafew@gmail.com>
2017-06-02 06:45:38 +0800

    Fixed the migration

--- database/migrations/1496338206813_update_transaction.js
+++ database/migrations/1496338206813_update_transaction.js
@@ -7,18 +7,18 @@ class UpdateTransactionTableSchema extends Schema {
   up () {
 
     this.table('transactions', (table) => {
-        table.dropColumn('action')
-        table.dropColumn('status')
-        table.dropColumn('acc_type')
+      table.dropColumn('action')
+      table.dropColumn('status')
+      table.dropColumn('acc_type')
     })
 
     this.table('transactions', (table) => {
-        table.integer('orderbook_id').after('uuid').notNullable()
-        // table.enum('asset', ['TRC','BTC','ETH','NONE']).after('orderbook_id').defaultTo('NONE')
-        // table.enum('to_asset', ['TRC','BTC','ETH','NONE']).after('asset').defaultTo('NONE')
-        table.enum('action', ['BUY', 'SELL', 'TRANSFER']).after('orderbook_id').notNullable()
-        table.enum('status', ['SUCCESS', 'FAILED', 'PENDING']).after('action').defaultTo('PENDING').notNullable()
-        table.string('trace', 10).after('status').defaultTo(1)
+      table.integer('orderbook_id').after('uuid').unsigned().references('id').inTable('order_books')
+      // table.enum('asset', ['TRC','BTC','ETH','NONE']).after('orderbook_id').defaultTo('NONE')
+      // table.enum('to_asset', ['TRC','BTC','ETH','NONE']).after('asset').defaultTo('NONE')
+      table.enum('action', ['BUY', 'SELL', 'TRANSFER', 'NONE']).after('orderbook_id').notNullable().defaultTo('NONE')
+      table.enum('status', ['SUCCESS', 'FAILED', 'PENDING']).after('action').defaultTo('PENDING').notNullable()
+      table.string('trace', 10).after('status').defaultTo('START')
     })
   }
 
