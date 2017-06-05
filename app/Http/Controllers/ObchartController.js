e02101bb5773a5379aa755efb72e938e851d6722
faiz <anwar.faiz@gmail.com>
2017-06-05 23:15:07 +0800

    mdepth chart jsonp callback not done

--- app/Http/Controllers/ObchartController.js
+++ app/Http/Controllers/ObchartController.js
@@ -24,18 +24,40 @@ class ObchartController {
     // console.log('bid:', bid)
     // console.log(bid, ask)
     // const data = {bid: bid, ask: ask}
-    console.log(ask)
 
-    yield response.sendView('orderbook.chart',{
-      bid_data : bid,
-      ask_data : ask,
+    // yield response.sendView('orderbook.chart',{
+    //   bid_data : bid,
+    //   ask_data : ask,
 
-    })
+    // })
+
+
+
     // const resp = `callback=([`, ask, bid,`]);`
     // console.log('resp:', resp)
 
-    // response.jsonp({bid: bid, ask: ask})
+    response.jsonp({bid: bid, ask: ask})
     // return(bid,ask)
+  }
+
+  * bid (request, response) {
+    const bid = yield Database.select('price', 'amount')
+    .from('order_books')
+    .where('type','bid')
+    .where('status','ACTIVE')
+    .orderBy('price', 'asc')
+    .groupBy('price')
+    response.jsonp(bid)
+  }
+
+  * ask (request, response) {
+    const ask = yield Database.select('price', 'amount')
+    .from('order_books')
+    .where('type', 'ask')
+    .where('status','ACTIVE')
+    .orderBy('price', 'asc')
+    .groupBy('price')
+    response.jsonp(ask)
   }
   
 }
