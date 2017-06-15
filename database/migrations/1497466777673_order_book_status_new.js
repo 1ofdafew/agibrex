1fe3638d2daa8bb7038385b98385ad2884d7e04e
Da Few <1ofdafew@gmail.com>
2017-06-15 12:19:46 +0800

    Added from/to address

--- database/migrations/1497466777673_order_book_status_new.js
+++ database/migrations/1497466777673_order_book_status_new.js
@@ -7,7 +7,8 @@ class OrderBookStatusNewTableSchema extends Schema {
   up () {
     this.table('order_books', (table) => {
       table.enum('status', ['NEW', 'ACTIVE', 'CANCELLED', 'PENDING', 'CLOSED']).after('price').defaultTo('NEW').notNullable().alter()
-      table.string('address', 120).after('to_asset')
+      table.string('from_address', 120).after('asset')
+      table.string('to_address', 120).after('to_asset')
     })
   }
 
